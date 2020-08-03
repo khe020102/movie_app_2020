3bc7ca39ef538610f93404ad6d77771a078b0051
khe020102 <68580600+khe020102@users.noreply.github.com>
2020-08-03 16:45:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
+
+
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+  getMovies = async () => {
+    const {
+      data: {
+        data: { movies },
+      },
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false });
+  };
   componentDidMount() {
-    // 영화 데이터 로딩!
-    setTimeout(() => {
-      this.setState({ isLoading: false });
-    }, 6000);
+    this.getMovies();
   }
-  render() {
-    const { isLoading } = this.state;
-    return <div>{isLoading ? 'Loading...' : 'We are ready'}</div>;
-  }
-}
-export default App;
