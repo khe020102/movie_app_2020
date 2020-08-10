2229b19d1fcc08d0bf12441a57876c7b65684672
김효은 <gimhyoeun@gimhyoeun.local>
2020-08-10 20:42:18 +0900

    Updates

---
+++ precache-manifest.f60de3607b0998deccae1c86710f91c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d76eeea31292b96f0dc9dd8ef4d2c3ea",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "5f87b19b85704fc8f842",
+    "url": "/movie_app_2020/static/css/main.bfff854d.chunk.css"
+  },
+  {
+    "revision": "bb38ca0ac903b612bc15",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.cdc3c92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f87b19b85704fc8f842",
+    "url": "/movie_app_2020/static/js/main.f9286ab1.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
